e0cc7fafb9f36e90541fad9eb5ed70329d4fcb49
Yogesh kumar <yogeshkumar12112003@gmail.com>
2024-09-09 09:20:39 +0530

    variables and diffrence

---
+++ variables-in-js/app.js
@@ -0,0 +1,17 @@
+// let firstName = 'Yogesh'
+// let lastName = 'Kumar'
+// let age = 21
+// let isHappy = true
+
+
+// let userIntro = 'My Name is ' + firstName + ' ' + lastName + '. I am' + ' ' + age + ' ' + 'year old , and i am ' + isHappy + '.'
+
+
+// let y = 5;
+//  let y = 10; // Uncaught SyntaxError: Identifier 'y' has already been declared
+
+// var x = 5;
+// var x = 10; // no error we can reassign the value
+
+// const z = 5;
+//  z = 10; // Uncaught TypeError: Assignment to constant variable.
