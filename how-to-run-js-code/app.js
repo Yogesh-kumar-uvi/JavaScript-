9f913a08fa093250620f2457723250598123262e
Yogesh kumar <yogeshkumar12112003@gmail.com>
2024-09-10 19:07:24 +0530

    how to run code in js (Hoisting)

---
+++ how-to-run-js-code/app.js
@@ -0,0 +1,15 @@
+// debugger
+
+ console.log(firstName)
+// console.log(lastName)
+var firstName = 'Akash'
+let lastName = 'Singh'
+let age = 15
+const yearOfBirth = 1999
+
+
+
+// let userIntro = 'Hi, my name is ' + firstName + ' ' + lastName
+
+
+
