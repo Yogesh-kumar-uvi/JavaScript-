51e9126bc05b816736340e549cc0306df423d97d
Yogesh kumar <yogeshkumar12112003@gmail.com>
2024-12-13 19:14:23 +0530

    objects

--- 17-objects-in-js/app.js
+++ 17-objects-in-js/app.js
@@ -1,5 +1,34 @@
+// const user = {
+//     name: 'yogesh',
+//     age: 21,
+//     gender: 'male',
+// }
+// console.log(user.name)
+// console.log(user["age"])
+
+
+
 const user = {
-    name: 'yogesh',
+    firstName: 'yogesh',
+    lastName: 'kumar',
     age: 21,
     gender: 'male',
-}
+    education:{
+        degree: 'B.tech',
+        university: 'aktu',
+        cgpa: 7.5
+    }
+};
+
+// Adding a new property
+user["is-student"] = true;
+
+// Accessing object values
+console.log(user.firstName);  // Output: yogesh
+console.log(user["age"]);     // Output: 21
+
+// Updating object values
+user.firstName = 'Yuvi';      // Update firstName
+console.log(user.firstName);  // Output: Yuvi
+
+
